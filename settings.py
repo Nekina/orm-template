db54ae17c9802b3eb06edbea40e670a2b47f8345
Nekina <joshuaacusta@gmail.com>
2025-06-30 08:14:35 -0400

    Completed template

---
+++ settings.py
@@ -0,0 +1,17 @@
+# PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'nN5Rr0oU6G9U27NAi1oCEZC3',
+        'HOST': 'postgres',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'standalone',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
